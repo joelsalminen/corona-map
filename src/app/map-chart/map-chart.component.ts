f0b9fca78897762adfc5c36d58fccd9cceff4370
Joel Salminen <joel.salminen@indoorinformatics.com>
2020-04-25 11:44:48 +0300

    MapChartComponent: populateMap refactored.

--- src/app/map-chart/map-chart.component.ts
+++ src/app/map-chart/map-chart.component.ts
@@ -15,33 +15,25 @@ export class MapChartComponent implements OnInit {
   constructor() {}
 
   populateMap(countryData: ICountry[]): void {
-    // Create map instance
     var chart = am4core.create('chartdiv', am4maps.MapChart);
 
-    // Set map definition
     chart.geodata = am4geodata_worldLow;
-
-    // Set projection
     chart.projection = new am4maps.projections.Miller();
 
-    // Create map polygon series
     var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-    // Make map load polygon (like country names) data from GeoJSON
     polygonSeries.useGeodata = true;
 
-    // Configure series
     var polygonTemplate = polygonSeries.mapPolygons.template;
     polygonTemplate.tooltipText = '{name}';
     polygonTemplate.fill = am4core.color('#999');
 
-    // Create hover state and set alternative fill color
     var hs = polygonTemplate.states.create('hover');
     hs.properties.fill = am4core.color('#ed8f1c');
 
     // Remove Antarctica
     polygonSeries.exclude = ['AQ'];
     polygonTemplate.tooltipText = '{name}: {value}';
+
     // Add heat rule
     polygonSeries.heatRules.push({
       property: 'fill',
