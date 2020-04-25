25599c3dd4ca12bd6541ac6a0fba5f273e7c99de
Joel Salminen <joel.salminen@indoorinformatics.com>
2020-04-25 11:21:32 +0300

    MapChartComponent generated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoronaComponent } from './corona/corona.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MapChartComponent } from './map-chart/map-chart.component';
 
 @NgModule({
   imports: [BrowserModule, HttpClientModule],
-  declarations: [AppComponent, CoronaComponent],
+  declarations: [AppComponent, CoronaComponent, MapChartComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
