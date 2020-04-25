5d7f21a06c3060b83894b5155ad13f72838ba770
Joel Salminen <joel.salminen@indoorinformatics.com>
2020-04-25 10:54:53 +0300

    Countries interface added.

---
+++ src/app/map-chart/countries.ts
@@ -0,0 +1,12 @@
+export interface ICountry {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
